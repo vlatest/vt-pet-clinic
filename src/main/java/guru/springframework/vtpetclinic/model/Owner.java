a92d0b888d9674b630588758112c37ebf2d09ac9
vlatest <knezevicvladimir@yahoo.com>
2018-10-27 18:13:39 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/vtpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.vtpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/vtpetclinic/model/PetType.java
@@ -0,0 +1,10 @@
+package guru.springframework.vtpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+}
