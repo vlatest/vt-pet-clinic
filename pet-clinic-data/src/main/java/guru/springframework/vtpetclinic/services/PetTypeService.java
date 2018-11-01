51a2dea2316aa051daeea856203e6e21c2e49bab
vlatest <knezevicvladimir@yahoo.com>
2018-11-01 17:49:09 +0100

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/vtpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.vtpetclinic.services;
+
+import guru.springframework.vtpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
