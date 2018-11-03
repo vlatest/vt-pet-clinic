adbed387274befeea5b2cadceb193e85ed9cce90
vlatest <knezevicvladimir@yahoo.com>
2018-11-03 16:16:10 +0100

    Updated owner  with JPA annotations. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/vtpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/vtpetclinic/model/PetType.java
@@ -1,12 +1,15 @@
 package guru.springframework.vtpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
-
-    public PetType() {
-
-    }
 
     public String getName() {
         return name;
